42bde9648fa9ce610869cee3d0e1a1dae59f6a6c
Estevão <estevao.prado.2019@mumail.ie>
2021-04-19 21:20:10 -0300

    test

--- BARTreatEff/R/modify_tree.R
+++ BARTreatEff/R/modify_tree.R
@@ -121,8 +121,8 @@ grow_tree = function(X, y, curr_tree, node_min_size, s) {
                            prob = as.integer(terminal_node_size > node_min_size)) # Choose which node to split, set prob to zero for any nodes that are too small
 
     # Choose a split variable uniformly from all columns (the first one is the intercept)
-    split_variable = sample(2:ncol(X), 1, prob = s)
-
+    # split_variable = sample(2:ncol(X), 1, prob = s)
+    split_variable = sample(1:ncol(X), 1, prob = s)
     # Alternatively follow BARTMachine and choose a split value using sample on the internal values of the available
     available_values = sort(unique(X[new_tree$node_indices == node_to_split,
                                      split_variable]))
